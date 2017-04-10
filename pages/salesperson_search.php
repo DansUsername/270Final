11de763cab6e42345a7a8fa18ddcc9be3185a755
davidcolling <davidcolling1@gmail.com>
2017-04-10 16:54:08 -0500

    Made result.php
    and made form elements left-aligned

--- pages/salesperson_search.php
+++ pages/salesperson_search.php
@@ -16,9 +16,15 @@
 <div class="block">
 	<p class="block_title big">Salesperson Search</p>
     <div class="media">
-		<form action="welcome.php" method="get">
+		<form action="result.php" method="get">
 			<p class="little">Location</p><br>
-			<input type="text">
+			<input type="text" name="location">
+			
+			<p class="little">Vehicle</p><br>
+			<input type="text" name="vehicle">
+			
+			<br>
+			<input type="submit" value="Search" class="link_button">
 		</form>
 	</div>
 </div>
