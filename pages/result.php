66265a93f47a09af80d12e17989fc3da16a4e0e1
davidcolling <davidcolling1@gmail.com>
2017-04-12 17:52:58 -0500

    Added testDB example code to result.php

--- pages/result.php
+++ pages/result.php
@@ -1,4 +1,32 @@
 <?php
-	echo "The location is " . $_GET["location"];
-	echo "The vehicle is " . $_GET["vehicle"];
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "car_dealership";
+	$port = 3306;
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Connected successfully<br>";
+
+	$sql = "select * from vehicle";
+	$result = $conn->query($sql);
+
+	if($result->num_rows > 0) {
+		echo "There are results.";
+	} else{
+		echo "There are no results.";
+	}
+	echo "<br>";
+
+	$sql = "select * from vehicle";
+	echo $sql;
+
+	echo "The location is " . $_GET["location"] . "<br>";
+	echo "The vehicle is " . $_GET["vehicle"] . "<br>";
 ?>
