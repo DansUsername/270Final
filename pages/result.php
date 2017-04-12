8f0af1cac3b2b9b47ba3e7bca85c3e3e19cb401c
davidcolling <davidcolling1@gmail.com>
2017-04-12 18:05:44 -0500

    Defined parts of the result page

--- pages/result.php
+++ pages/result.php
@@ -4,16 +4,14 @@
 	$password = "";
 	$dbname = "car_dealership";
 	$port = 3306;
-	
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
-
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	} 
 	echo "Connected successfully<br>";
-
+	//testing sql
 	$sql = "select * from vehicle";
 	$result = $conn->query($sql);
 
@@ -24,9 +22,10 @@
 	}
 	echo "<br>";
 
-	$sql = "select * from vehicle";
-	echo $sql;
-
+	$sql = "select * from vehicle<br>";
+	$result = $conn->query($sql);
+	echo $sql;	
+	//Items from form
 	echo "The location is " . $_GET["location"] . "<br>";
 	echo "The vehicle is " . $_GET["vehicle"] . "<br>";
 ?>
